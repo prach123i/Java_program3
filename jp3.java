286e4ec549a65417d08ce7b6d0e93798945bd603
Prachi <151402023+prach123i@users.noreply.github.com>
2024-08-21 10:00:30 +0530

    Add files via upload

---
+++ jp3.java
@@ -0,0 +1,20 @@
+class Animal {
+    void eat() {
+        System.out.println("This animal eats food.");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("The dog barks.");
+    }
+}
+
+public class jp3 {
+    public static void main(String[] args){
+        Dog myDog = new Dog();
+        
+        myDog.eat();  
+        myDog.bark();
+    }
+}
